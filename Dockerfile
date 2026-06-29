f6c6caf0c23de9814c4f168d1d47502d84edda21
KimDoYoung <kdy987@gmail.com>
2026-06-29 10:36:02 +0900

    배포 1

--- Dockerfile
+++ Dockerfile
@@ -17,6 +17,9 @@ RUN rm -rf $CATALINA_HOME/webapps/ROOT
 # 컨텍스트 내의 aman.war 파일을 톰캣 경로로 복사
 COPY aman.war $CATALINA_HOME/webapps/aman.war
 
+# non-root 일반 사용자(1000:1000)가 webapps, work, temp, logs 및 데이터 폴더에 쓸 수 있도록 소유권 변경
+RUN chown -R 1000:1000 $CATALINA_HOME /data/docker/apps/aman
+
 EXPOSE 8080
 
 CMD ["catalina.sh", "run"]
--- backend/src/main/java/kr/co/kfs/aman/AmanApplication.java
+++ backend/src/main/java/kr/co/kfs/aman/AmanApplication.java
@@ -2,9 +2,17 @@ package kr.co.kfs.aman;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class AmanApplication {
+public class AmanApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(AmanApplication.class);
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(AmanApplication.class, args);
     }
